3908540a36ca9504aca65a0233914820adea237f
redpandaathome <leeyumi415@gmail.com>
2022-11-09 11:57:10 +0900

    test - markdown renderere after asset pass

--- .eleventy.js
+++ .eleventy.js
@@ -3,22 +3,6 @@ const path = require("path");
 const markdown = require('markdown-it')()
 const {getImageOpt, processImage, generateImageHtml, getHtmlOpts, getImagePath, imageShortcode} = require('./imageProcesser.js');
 
-
-markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
-  const token = tokens[idx]
-  let imgSrc = token.attrGet('src')
-  const imgAlt = token.content
-  const imgTitle = token.attrGet('title')
-  
-  let {commonPath, src, inputFullPath, outputPath} = getImagePath(imgSrc);
-  
-  const htmlOpts = getHtmlOpts(imgTitle, imgAlt);
-  const imgOpts = getImageOpt(commonPath, outputPath);
-  const metadata = processImage(src, imgOpts, inputFullPath);
-  return generateImageHtml(metadata, htmlOpts);
-}
-
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css')
   eleventyConfig.addPassthroughCopy('./src/assets')
@@ -26,6 +10,21 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
+    const token = tokens[idx]
+    let imgSrc = token.attrGet('src')
+    const imgAlt = token.content
+    const imgTitle = token.attrGet('title')
+    
+    let {commonPath, src, inputFullPath, outputPath} = getImagePath(imgSrc);
+    
+    const htmlOpts = getHtmlOpts(imgTitle, imgAlt);
+    const imgOpts = getImageOpt(commonPath, outputPath);
+    const metadata = processImage(src, imgOpts, inputFullPath);
+    return generateImageHtml(metadata, htmlOpts);
+  }
+
   eleventyConfig.addShortcode('image', imageShortcode);  
   eleventyConfig.setLibrary('md', markdown)
   return {
