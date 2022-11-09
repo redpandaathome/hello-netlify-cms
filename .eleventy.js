4e07880a29c8bddd566a0028c9c09bb58a6fd968
redpandaathome <leeyumi415@gmail.com>
2022-11-09 11:30:47 +0900

    code refactoring - image processer

--- .eleventy.js
+++ .eleventy.js
@@ -1,83 +1,8 @@
 const { DateTime } = require("luxon");
-const Image = require("@11ty/eleventy-img");
 const path = require("path");
 const markdown = require('markdown-it')()
+const {getImageOpt, processImage, generateImageHtml, getHtmlOpts, getImagePath, imageShortcode} = require('./imageProcesser.js');
 
-function getImageOpt(inputPath, outputPath){
-  return {
-    widths: [320, 560, 800, 1040],
-    formats: [null], //multiple types like "webp", null will generate <picture>tag in html
-    urlPath: inputPath, ///assets/blog
-    outputDir: outputPath, //public/assets/blog
-    filenameFormat: function (id, src, width, format, options) {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-  
-      return `${name}-${width}.${format}`;
-    }
-  }
-}
-
-function processImage(imgSrc, imgOpts, inputFullPath){
-  try{
-    Image(imgSrc, imgOpts)
-  }catch(error){
-    console.log('🚨 error...:', error);
-  }
-  
-  let metadata;
-  try{
-    metadata = Image.statsSync(inputFullPath, imgOpts)
-  }catch(error){
-    console.log('error:', error);
-  }
-  console.log('🐛🐛🐛🐛🐛🐛🐛🐛 af metadata- METADATA:', metadata);
-  return metadata;
-}
-
-function generateImageHtml(metadata, htmlOpts){
-  const generated = Image.generateHTML(metadata, {
-    sizes: '(max-width: 768px) 100vw, 768px',
-    ...htmlOpts
-  })
-  console.log('🤡🤡🤡🤡🤡:', generated);
-  return generated;
-}
-
-function getHtmlOpts(imgTitle, imgAlt){
-  return {
-    title: imgTitle,
-    alt: imgAlt,
-    loading: 'lazy',
-    decoding: 'async'
-  }
-}
-
-const imageShortcode = function (
-  imgSrc,
-  alt,
-  className = undefined,
-  widths= [320, 560, 800, 1040],
-  formats= [null],
-  sizes = '100vw'
-) {
-  console.log('👀👀👀👀👀👀👀 imageShortcode...:', );
-  
-
-  let commonPath = path.dirname(imgSrc);
-  imgSrc = path.join("./src", imgSrc)
-  let imgTitle = path.parse(imgSrc).base
-  let imgAlt = alt;
-
-  let inputFullPath = path.join(path.join("./src", commonPath), path.parse(imgSrc).base);
-  let outputPath = path.join("./public", commonPath)
-  let htmlOpts = getHtmlOpts(imgTitle, imgAlt);
-  const imgOpts = getImageOpt(commonPath, outputPath);
-  const metadata = processImage(imgSrc, imgOpts, inputFullPath);
-  let generated = generateImageHtml(metadata, htmlOpts);
-  console.log('🌻🌻🌻 generatd...:', generated);
-  return generated
-};
 
 markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
   const token = tokens[idx]
@@ -85,14 +10,11 @@ markdown.renderer.rules.image = function (tokens, idx, options, env, self) {
   const imgAlt = token.content
   const imgTitle = token.attrGet('title')
   
-  let commonPath = path.dirname(imgSrc);
-  imgSrc = path.join("./src", imgSrc)
-  let inputFullPath = path.join(path.join("./src", commonPath), path.parse(imgSrc).base);
-  let outputPath = path.join("./public", commonPath)
+  let {commonPath, src, inputFullPath, outputPath} = getImagePath(imgSrc);
   
   const htmlOpts = getHtmlOpts(imgTitle, imgAlt);
   const imgOpts = getImageOpt(commonPath, outputPath);
-  const metadata = processImage(imgSrc, imgOpts, inputFullPath);
+  const metadata = processImage(src, imgOpts, inputFullPath);
   return generateImageHtml(metadata, htmlOpts);
 }
 
@@ -104,11 +26,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
-  eleventyConfig.addShortcode('image', imageShortcode);
-  // eleventyConfig.addNunjucksShortcode('image', imageShortcode);
-  
-  
-  // eleventyConfig.setLibrary('md', markdown)
+  eleventyConfig.addShortcode('image', imageShortcode);  
+  eleventyConfig.setLibrary('md', markdown)
   return {
     dir: {
       input: "src",
---
+++ imageProcesser.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const Image = require("@11ty/eleventy-img");
+
+function getImageOpt(inputPath, outputPath) {
+    return {
+        widths: [320, 560, 800, 1040],
+        formats: [null], //multiple types like "webp", null will generate <picture>tag in html
+        urlPath: inputPath, ///assets/blog
+        outputDir: outputPath, //public/assets/blog
+        filenameFormat: function (id, src, width, format, options) {
+            const extension = path.extname(src);
+            const name = path.basename(src, extension);
+
+            return `${name}-${width}.${format}`;
+        }
+    }
+}
+
+function processImage(imgSrc, imgOpts, inputFullPath) {
+    try {
+        Image(imgSrc, imgOpts)
+    } catch (error) {
+        console.log('🚨 error...:', error);
+    }
+
+    let metadata;
+    try {
+        metadata = Image.statsSync(inputFullPath, imgOpts)
+    } catch (error) {
+        console.log('error:', error);
+    }
+    // console.log('🐛🐛🐛🐛🐛🐛🐛🐛 af metadata- METADATA:', metadata);
+    return metadata;
+}
+
+function generateImageHtml(metadata, htmlOpts) {
+    const generated = Image.generateHTML(metadata, {
+        sizes: '(max-width: 768px) 100vw, 768px',
+        ...htmlOpts
+    })
+    // console.log('🤡🤡🤡🤡🤡:', generated);
+    return generated;
+}
+
+function getHtmlOpts(imgTitle, imgAlt) {
+    return {
+        title: imgTitle,
+        alt: imgAlt,
+        loading: 'lazy',
+        decoding: 'async'
+    }
+}
+
+function getImagePath(imgSrc) {
+    let commonPath = path.dirname(imgSrc);
+    imgSrc = path.join("./src", imgSrc)
+    let inputFullPath = path.join(path.join("./src", commonPath), path.parse(imgSrc).base);
+    let outputPath = path.join("./public", commonPath)
+
+    return { commonPath, src: imgSrc, inputFullPath, outputPath }
+}
+
+const imageShortcode = function (
+    imgSrc,
+    alt,
+    className = undefined,
+    widths = [320, 560, 800, 1040],
+    formats = [null],
+    sizes = '100vw'
+) {
+    let { commonPath, src, inputFullPath, outputPath } = getImagePath(imgSrc);
+
+    let htmlOpts = getHtmlOpts(path.parse(src).base, alt);
+    const imgOpts = getImageOpt(commonPath, outputPath);
+    const metadata = processImage(src, imgOpts, inputFullPath);
+    let generated = generateImageHtml(metadata, htmlOpts);
+    // console.log('🌻🌻🌻 generatd...:', generated);
+    return generated
+};
+
+module.exports = {
+    getImageOpt,
+    processImage,
+    generateImageHtml,
+    getHtmlOpts,
+    getImagePath,
+    imageShortcode
+}
